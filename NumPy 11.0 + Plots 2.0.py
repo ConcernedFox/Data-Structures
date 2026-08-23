a409188738fb0ba4ba9f97207186402a59c36b37
ConcernedFox <vihaan.gururani@gmail.com>
2026-08-23 09:32:52 -0400

    Add files via upload

--- NumPy 11.0 + Plots 2.0.py
+++ NumPy 11.0 + Plots 2.0.py
@@ -1,3 +1,15 @@
-import matplotlib as mp
+import matplotlib.pyplot as plots
 import numpy as np
 
+Listx = np.arange(-5, 5, 0.1)
+print(Listx)
+
+Listy = Listx
+Listz = Listx*Listx
+Listw = Listz*Listx
+plots.plot(Listx, Listy)
+plots.show()
+plots.plot(Listx, Listz)
+plots.show()
+plots.plot(Listx, Listw)
+plots.show()
